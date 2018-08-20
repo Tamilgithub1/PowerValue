e65f816684ca55c2dec94cd34108f9f443fb4f54
Tamilgithub1 <31219547+Tamilgithub1@users.noreply.github.com>
2018-08-20 16:24:40 +0530

    Create PowerValue.java

---
+++ PowerValue.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class test {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	int m=in.nextInt();
+	int r=(int)Math.pow(n, m);
+	System.out.println(r);
+}
+}
